6989ac4a4ac1e057220191093f07876024d99f77
me-gusta <consistent.level@gmail.com>
2022-06-06 19:06:59 +0300

    add logging

--- db/engine.py
+++ db/engine.py
@@ -14,7 +14,7 @@ session: Session = sessionmaker(engine, expire_on_commit=False)()
 
 async def create_tables():
     Base.metadata.create_all(engine)
-    root_logger('DB tables created or OK')
+    root_logger.info('DB tables created or OK')
     # async with engine.begin() as conn:
     #     await conn.run_sync(Base.metadata.create_all)
     # session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
